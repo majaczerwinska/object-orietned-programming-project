4ccef64b01714f5d7d9fddfff7aebb0dda37c8ed
Andreas Tsatsanis <a.tsatsanis@student.tudelft.nl>
2023-03-08 12:51:18 +0100

    more tests

---
+++ server/src/test/java/server/SomeControllerTest.java
@@ -0,0 +1,20 @@
+package server;
+
+import org.junit.jupiter.api.Test;
+import server.database.CardRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomeControllerTest {
+
+//    VisitCounterService v = new VisitCounterService();
+//    CardRepository c = new CardRepository();
+//    SomeController s = new SomeController();
+    @Test
+    void visit() {
+    }
+
+    @Test
+    void index() {
+    }
+}
