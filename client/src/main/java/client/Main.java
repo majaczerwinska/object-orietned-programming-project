d63018faa2a4b974b7118024879d57ad9e730325
Andreas Tsatsanis <a.tsatsanis@student.tudelft.nl>
2023-03-09 17:22:30 +0100

    client starts, im still confused about where xml files go...

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -26,9 +26,17 @@ import java.net.URISyntaxException;
 //import client.scenes.AddQuoteCtrl;
 //import client.scenes.MainCtrl;
 //import client.scenes.QuoteOverviewCtrl;
+import client.scenes.CardCtrl;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
+import javafx.scene.image.Image;
 public class Main extends Application {
 
 //    private static final Injector INJECTOR = createInjector(new MyModule());
@@ -46,21 +54,35 @@ public class Main extends Application {
 
     /**
      *
-     * @param primaryStage the primary stage for this application, onto which
+     * @param stage the primary stage for this application, onto which
      * the application scene can be set.
      * Applications may create other stages, if needed, but they will not be
      * primary stages.
      * @throws IOException -
      */
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage stage) throws IOException {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+            Scene scene = new Scene(root, 500, 400, Color.ANTIQUEWHITE);
+
+            stage.setTitle("main scene title goes here");
+
+            stage.setX(400);
+            stage.setY(200);
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("very sad exception :(\nin start method of client.main..");
+            e.printStackTrace();
+        }
 
 //        var overview = FXML.load(QuoteOverviewCtrl.class, "client", "scenes", "QuoteOverview.fxml");
 //        var add = FXML.load(AddQuoteCtrl.class, "client", "scenes", "AddQuote.fxml");
 //        var card = FXML.load(CardCtrl.class, "client", "scenes", "Card.fxml");
 //        var mainCtrl = INJECTOR.getInstance(MainCtrl.class);
 //        mainCtrl.initialize(primaryStage, overview, add, card);
-
 
     }
 }
