6e4ea308ca746c89af753408b64010c09e9ae636
Andreas Tsatsanis <a.tsatsanis@student.tudelft.nl>
2023-03-23 11:31:31 +0100

    i suffered. but, too long later the main and config classes are tested, and the pipeline should be updated to separate the different package testing. before merging I will create test jobs for frontend so we can later toggle the required coverage if we manage to test it

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,1 +1,10 @@
-INSERT INTO BOARDS (id, name, boardkey, color) values (0, 'PublicBoard','main', 0);
+CREATE TABLE IF NOT EXISTS BOARDS (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50),
+    boardkey VARCHAR(50),
+    color INT,
+    password VARCHAR(255)
+);
+DELETE FROM BOARDS WHERE id = 0;
+INSERT INTO BOARDS (id, name, boardkey, color, password) VALUES
+    (0, 'PublicBoard','main', 0, '');
