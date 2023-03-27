e2a12a1327084f858f45956f123a1ff1b4c5069a
Tudor Damian <t.c.damian@student.tudelft.nl>
2023-03-27 16:37:51 +0200

    adding the help page and the controller

---
+++ client/src/main/java/client/scenes/HelpCtrl.java
@@ -0,0 +1,24 @@
+package client.scenes;
+
+import javax.inject.Inject;
+
+public class HelpCtrl {
+    private final MainCtrl mainCtrl;
+
+
+    /**
+     * help controller constructor method
+     * @param mainCtrl main controller
+     */
+    @Inject
+    public HelpCtrl(MainCtrl mainCtrl) {
+        this.mainCtrl = mainCtrl;
+    }
+
+    /**
+     * go back button handler method
+     */
+    public void goBackButtonHandler() {
+        mainCtrl.showLanding();
+    }
+}
