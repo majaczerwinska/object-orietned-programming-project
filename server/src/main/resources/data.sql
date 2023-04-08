0166907adffceb4055be47c6cd64313f091eb9f5
Andreas Tsatsanis <a.tsatsanis@student.tudelft.nl>
2023-04-08 21:39:18 +0200

    cleanup

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -7,21 +7,20 @@ CREATE TABLE IF NOT EXISTS BOARDS (
     password VARCHAR(255)
 );
 
+CREATE TABLE IF NOT EXISTS palettes (
+    palette_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    palette_name VARCHAR(250),
+    bgd_color INT,
+    font_color INT,
+    isdefault BIT,
+    FOREIGN KEY (board_id) REFERENCES BOARDS (id)
+);
+
 DELETE FROM BOARDS WHERE id = 0;
 
 INSERT INTO BOARDS (id, name, b_color, f_color, boardkey, password, listb, listt) VALUES
     (0, 'Public Board', 3368550,  16777215, 'public', '', 1723725, 16777215);
 
-CREATE TABLE IF NOT EXISTS palettes (
-        palette_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-        palette_name VARCHAR(250),
-        bgd_color INT,
-        font_color INT,
-        isdefault BIT,
-        FOREIGN KEY (board_id) REFERENCES BOARDS (id)
-    );
-
-    INSERT INTO palettes (palette_id, palette_name, bgd_color, font_color, isdefault, board_id) VALUES
-    (0, 'default', 16777215, 0, true, 0);
-
+INSERT INTO palettes (palette_id, palette_name, bgd_color, font_color, isdefault, board_id) VALUES
+(0, 'default', 16777215, 0, true, 0);
 
